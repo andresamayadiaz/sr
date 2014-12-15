9242c2e8974772171a0df0182305057b851b9c33
Kevin <kevin@kevinhq.com>
2014-12-15 19:41:37 +0700

    Fix counter of alertas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -178,27 +178,45 @@ class HomeController < ApplicationController
   
   private
     def set_notifications
+
       all_alertas = current_user.notifications
+      all_unread_alertas = all_alertas.select{|a|a.status==false}
+      all_advertencias = all_alertas.select{|a|a.category=='Warning'}
+      all_faltas = all_alertas.select{|a|a.category=='Error'}
+      all_validos = all_alertas.select{|a|a.category=='Success'}
+      all_actual = all_alertas.select{|a|a.created_at.month==Time.now.month}
+      all_anterior = all_alertas.select{|a|a.created_at.month==(Time.now-1.month).month}
+      all_leidos = all_alertas.select{|a|a.status==true}
+      
+      @unreads_count = all_unread_alertas.length
+      @advertencias_count = all_advertencias.length
+      @faltas_count = all_faltas.length
+      @validos_count = all_validos.length
+      @actual_count = all_actual.length
+      @anterior_count = all_anterior.length
+      @leidos_count = all_leidos.length
+      
       if params[:filter].present?
         case params[:filter]
         when 'advertencias'
-          @alertas = all_alertas.select{|a|a.category=='Warning'}
+          @alertas = all_advertencias
         when 'faltas'
-          @alertas = all_alertas.select{|a|a.category=='Error'}
+          @alertas = all_faltas
         when 'validos'
-          @alertas = all_alertas.select{|a|a.category=='Success'}
+          @alertas = all_validos
         when 'mes-actual'
-          @alertas = all_alertas.select{|a|a.created_at.month==Time.now.month}
+          @alertas = all_actual
         when 'mes-anterior'
-          @alertas = all_alertas.select{|a|a.created_at.month==(Time.now-1.month).month}
+          @alertas = all_anterior
         when 'leidos'
-          @alertas = all_alertas.select{|a|a.status==true}
+          @alertas = all_leidos
         end
       else
-        @alertas = all_alertas.select{|a|a.status==false}
+        @alertas = all_unread_alertas
       end
-        @alertas = @alertas.sort_by{|a|a.created_at} rescue []
-        @alertas = Kaminari.paginate_array(@alertas).page(params[:page])
+      @alertas = @alertas.sort_by{|a|a.created_at} rescue []
+      @alertas = Kaminari.paginate_array(@alertas).page(params[:page])
+
     end
 
     def set_vars
