7971d6b19aa6c7db03bc72123409893e15235a36
Andres Amaya Diaz <andres.amaya.diaz@gmail.com>
2015-03-03 17:21:24 -0600

    Fog Gem Config Change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,13 +7,14 @@ Rails.application.configure do
        :storage => :fog,
        :fog_credentials => {
           :provider           => 'Rackspace',
+          :rackspace_region    => :ord,
           :rackspace_username => ENV["RACKSPACE_USERNAME"],
           :rackspace_api_key  => ENV["RACKSPACE_APIKEY"],
           :persistent => false
        },
        :fog_directory => '',
        :fog_public => true,
-       :fog_host => ENV["RACKSPACE_HOST"]
+       #:fog_host => ENV["RACKSPACE_HOST"]
   })
   
   # Code is not reloaded between requests.
